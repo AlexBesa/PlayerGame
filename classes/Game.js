ca87920d8e130278b983ada63c6e38666e9bf462
Alexandru Besa <besaalex17@yahoo.com>
2020-05-26 01:26:31 +0300

    final version

--- classes/Game.js
+++ classes/Game.js
@@ -1,62 +1,28 @@
 class Game {
     constructor(containerMap) {
-      this.containerMap = containerMap;
+        this.containerMap = containerMap;
     }
-    startApp(){
+    startApp() {
         let player = new Player();
-        let obstacle1 = new Obstacle();
-        let obstacle2 = new Obstacle();
-        let obstacle3 = new Obstacle();
-        let obstacle4 = new Obstacle();
-        // console.log("*********",obstacle1)
-        obstacle1.setLocation();
-        obstacle2.setLocation();
-        obstacle3.setLocation();
-        obstacle4.setLocation();
-    
+        const obs = [];
+        for (let i = 0; i < 20; i++) {
+            obs.push(new Obstacle("yellow"));
+            obs[i].setLocation();
+            obs[i].divObstacle();
+        }
         window.setInterval(()=>{
             context.clearRect(0,0,gameContainer.width,gameContainer.height);
             player.notOutsideGameContainer();
-
-            obstacle1.divObstacle();
-            obstacle2.divObstacle();
-            obstacle3.divObstacle();
-            obstacle4.divObstacle();
-
             player.display();
-            
-            if(player.meetObstacle(obstacle1)){
-                player.reduceLife();
-                obstacle1.setLocation();  
-            }
-            if(player.meetObstacle(obstacle2)){
-                player.reduceLife();
-                obstacle2.setLocation();  
-            }
-            if(player.meetObstacle(obstacle3)){
-                player.reduceLife();
-                obstacle3.setLocation();  
-            }
-            if(player.meetObstacle(obstacle4)){
-                player.reduceLife();
-                obstacle4.setLocation();  
-            }
-        },200);
-        player.changeDirection();       
+            for(let i=0;i<obs.length;i++){
+                obs[i].divObstacle()
+                if(player.meetObstacle(obs[i])){
+                    player.reduceLife();
+                    obs[i].setLocation();  
+                }
+            } 
+        },250);
+        player.changeDirection(); 
+        player.changeDirection();
     }
 }
-
-// let obstacle1 = new Obstacle(
-//     {
-//         x:this.x,
-//         y:this.y
-//     },
-//     {
-//         x:this.x,
-//         y:this.y
-//     },
-//     {
-//         x:this.x,
-//         y:this.y
-//     },
-// );
--- classes/Obstacles.js
+++ classes/Obstacles.js
@@ -1,35 +1,16 @@
 class Obstacle {
-    constructor(){
+    constructor(color){
         this.x;
         this.y;
-        // this.body = [
-        //     {
-        //         x: this.x,
-        //         y: this.y,
-        //     },
-        //     {
-        //         x: this.x,
-        //         y: this.y,
-        //     },
-        //     {
-        //         x: this.x,
-        //         y: this.y,
-        //     }
-        // ]; 
+        this.color=color;
     }
     divObstacle(){
-        context.fillStyle="yellow";
+        context.fillStyle=this.color;
         context.fillRect(this.x,this.y,scale,scale);
     }
     setLocation(){
+        
         this.x = (Math.floor(Math.random()* rows)) *scale;
         this.y = (Math.floor(Math.random()* columns)) *scale;
-    } 
-    // createObstacles(){
-    //     for(let i=0;i < 3;i++){
-    //         var obX = (Math.floor(Math.random()* rows)) *scale;
-    //         var obY = (Math.floor(Math.random()* columns)) *scale;
-    //         this.body.push({"x":obX,"y":obY});
-    //     }  
-    // }     
+    }     
 }
